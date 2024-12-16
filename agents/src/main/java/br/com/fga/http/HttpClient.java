e0d685786dbf2b64e3be8ba8dd0a38108d3721b4
DouglasMonteles <silvadouglas328@gmail.com>
2024-12-15 23:23:48 -0300

    :sparkles: adicionando suporte para requisições POST

--- agents/src/main/java/br/com/fga/http/HttpClient.java
+++ agents/src/main/java/br/com/fga/http/HttpClient.java
@@ -1,15 +1,21 @@
 package br.com.fga.http;
 
+import br.com.fga.models.Truck;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URL;
 
 public class HttpClient {
 
-    public static void main(String[] args) {
-        get("http://localhost:8080/vehicles");
-    }
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+//    public static void main(String[] args) {
+//        post("http://localhost:8080/vehicles", new Truck(10));
+//    }
 
     public static void get(String path) {
         try {
@@ -30,6 +36,31 @@ public class HttpClient {
         }
     }
 
+    public static void post(String path, Object data) {
+        try {
+            URI uri = new URI(path);
+            URL url = uri.toURL();
+            HttpURLConnection conn = getHttpURLConnection(url, "POST");
+
+            String jsonInput = mapper.writeValueAsString(data);
+
+            try (OutputStream os = conn.getOutputStream()) {
+                byte[] input = jsonInput.getBytes("utf-8");
+                os.write(input, 0, input.length);
+            }
+
+            // Verificando a resposta
+            int responseCode = conn.getResponseCode();
+            System.out.println("Código de resposta: " + responseCode);
+
+            assert responseCode == 200;
+
+            conn.disconnect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     private static HttpURLConnection getHttpURLConnection(URL url, String method) throws IOException {
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 
@@ -37,14 +68,6 @@ public class HttpClient {
         conn.setRequestProperty("Content-Type", "application/json");
         conn.setDoOutput(true);
 
-        // Corpo da requisição
-//        String jsonInput = "{\"key\":\"value\"}";
-
-        // Enviando os dados
-//        try (OutputStream os = conn.getOutputStream()) {
-//            byte[] input = jsonInput.getBytes("utf-8");
-//            os.write(input, 0, input.length);
-//        }
         return conn;
     }
 
--- server/src/main/java/br/com/fga/controllers/VehicleController.java
+++ server/src/main/java/br/com/fga/controllers/VehicleController.java
@@ -2,15 +2,14 @@ package br.com.fga.controllers;
 
 import br.com.fga.mock.SimulatorData;
 import br.com.fga.models.Simulation;
+import br.com.fga.models.Truck;
 import br.com.fga.models.Vehicle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.HtmlUtils;
 
 import java.util.List;
@@ -26,6 +25,12 @@ public class VehicleController {
     public ResponseEntity<List<Simulation>> vehicles() {
         simpMessagingTemplate.convertAndSend("/topic/updates", "{\"message\": \"teste\"}");
         return ResponseEntity.ok().body(SimulatorData.SIMULATION_LIST);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> receiveData(@RequestBody Truck truck) {
+        System.out.println(truck);
+        return ResponseEntity.ok().build();
     }
 
     @MessageMapping("/hello")
